bf6b96d1285d7a58822e937e66e76fc21ae63788
Edgar Barrón Monsivais <you@example.com>
2021-04-01 23:15:59 -0600

    Ultima actualizacion

--- vistas/profesores-Inicio.php
+++ vistas/profesores-Inicio.php
@@ -23,6 +23,33 @@ if(isset($_SESSION['Usuario'])){
   </script>";
 }
 
+//Comprobamos si esta definida la sesión 'tiempo'.
+if(isset($_SESSION['tiempo']) ) {
+
+  //Tiempo en segundos para dar vida a la sesión.
+  $inactivo = 10;//20min en este caso.
+
+  //Calculamos tiempo de vida inactivo.
+  $vida_session = time() - $_SESSION['tiempo'];
+
+      //Compraración para redirigir página, si la vida de sesión sea mayor a el tiempo insertado en inactivo.
+      if($vida_session > $inactivo)
+      {
+          //Removemos sesión.
+          session_unset();
+          //Destruimos sesión.
+          session_destroy();    
+        
+          //Redirigimos pagina.
+          header("Location: index.php");
+
+          exit();
+      }
+
+    }
+
+    $_SESSION['tiempo'] = time();
+
 ?>
 
 
